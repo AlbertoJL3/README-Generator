ba1061b61743ca3f682776f1a00bd3e751c3b9be
Beto Lopez <363alberto@gmail.com>
2022-11-28 19:31:38 -0400

    Added README/Fixed AppendToPage function

---
+++ README.md
@@ -0,0 +1,29 @@
+
+    NOTE: This project uses the MIT license!
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Description
+The goal of this project is to make a command-line application that will automatically format a .md file with user inputted information! Ideal usage is for creating READMEs for future projects! 
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#Questions)
+## Installation
+To install just use "git clone [repo link]" in the chosen directory and run by doing "node index.js" in your terminal! 
+## Usage
+This project should be used by cloning the git repo down to your local IDE and running the index.js file!
+## License
+This project uses the following license: 
+MIT
+## Contributing
+There are no contribution guidelines for this project yet! 
+## Tests 
+There are no test instructions for this project yet!
+## Questions
+Here is some contact information to find me! 
+
+[Github](https://www.github.com/AlbertoJL3)
+
+Email: 363alberto@gmail.com
--- index.js
+++ index.js
@@ -20,10 +20,37 @@ inquirer
 
         },
         {
-            type: 'checkbox',
+            type: 'input',
+            message: 'Please write the installation information for your project.',
+            name: 'installation',
+
+        },
+        {
+            type: 'list',
             message: 'What kind of license will you be using?',
             name: 'license',
-            choices: ["MIT", "ISC", "Microsoft Public License"]
+            default: "MIT",
+            choices: [
+                {
+                    name: "MIT",
+                    value: "MIT",
+                    value2: "MIT",
+                    value3: "MIT"
+                },
+                {
+                    name: "ISC",
+                    value: "ISC",
+                    value2: "ISC",
+                    value3: "ISC"
+
+                },
+                {
+                    name: "Microsoft Public License",
+                    value: "Microsoft_Public_License",
+                    value2: "Microsoft_Public_License",
+                    value3: "Microsoft-Public-License",
+                },
+            ],
         },
         {
             type: 'input',
@@ -33,7 +60,7 @@ inquirer
         },
         {
             type: 'input',
-            message: 'Please write the test instrucitons for your project.',
+            message: 'Please write the test instructions for your project.',
             name: 'test',
         },
         {
@@ -54,7 +81,8 @@ inquirer
 //Creates README file
 function appendToPage(data) {
     fs.writeFile(data.title + '.md', `
-[![License: `+ data.license +`](https://img.shields.io/badge/License-`+ data.license +`-yellow.svg)](https://opensource.org/licenses/`+ data.license +`)
+    NOTE: This project uses the ${data.license} license!
+[![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-yellow.svg)](https://opensource.org/licenses/${data.license})
 ## Description
 ${data.description}
 ## Table of Contents
@@ -74,35 +102,15 @@ ${data.license}
 ## Contributing
 ${data.contributing}
 ## Tests 
-${data.tests}
+${data.test}
 ## Questions
-${data.questions}
-Github information:
-github.com/${data.Github}
+Here is some contact information to find me: 
+
+[Github](https://www.github.com/${data.Github})
+
 Email: ${data.email}`,
         function (err) {
             if (err) throw err;
             console.log('Index.html Created!');
         });
 }
-
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-
-
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
